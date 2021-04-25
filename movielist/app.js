f1a2833ec8ff915f3d44e6622f76ae196d71bc5f
mu <ajghmcvu@gmail.com>
2021-04-25 13:19:35 +0800

    second update

--- movielist/app.js
+++ movielist/app.js
@@ -1,18 +1,37 @@
+// require packages used in the project
 const express = require('express')
-const app =express()
+const app = express()
 const port = 3000
-
 const exphbs = require('express-handlebars')
+const movieList = require('./movie.json')
 
-app.engine('handlebars', exphbs({defaultLayout:'main'}))
+// setting template engine
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
+// setting static files
 app.use(express.static('public'))
 
-app.get('/',(req, res)=>{
-  res.render('index')
+app.get('/search', (req, res) => {
+  const keyword = req.query.keyword
+  const movie = movieList.results.filter(movie => {
+    return movie.title.toLowerCase().includes(keyword.toLowerCase())
+  })
+  res.render('index', { movie: movie, keyword: keyword })
 })
 
-app.listen(port,()=>{
+// routes setting
+app.get('/', (req, res) => {
+  // past the movie data into 'index' partial template
+  res.render('index', { movie: movieList.results })
+})
+
+app.get('/movie/:movie_id', (req, res) => {
+   const movie = movieList.results.find(movie => movie.id.toString() === req.params.movie_id)
+  res.render('show', { movie: movie })
+})
+
+// start and listen on the Express server
+app.listen(port, () => {
   console.log(`Express is listening on localhost:${port}`)
 })
